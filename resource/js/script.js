48a381f646f2ff6944b9e8148dd483817e2646a7
Mahsa <mahsahaddadi19@gmail.com>
2022-07-01 10:56:31 +1000

    test2

---
+++ resource/js/script.js
@@ -0,0 +1,57 @@
+//Validate name
+
+function validateName() {
+  let username = document.getElementById("name");
+
+  if (username.value.length > 8 || username.value == "") {
+    alert("Input invalid");
+  } else {
+    alert("Success");
+  }
+}
+let submitButton = document.getElementById("submitBTN");
+submitButton.addEventListener("click", validateName);
+
+//Description
+
+function displayDescription() {
+  let descriptionBox = document.getElementById("description");
+
+  if (descriptionBox.value === "" || descriptionBox.value.length > 15) {
+    alert("Please write your description!");
+  } else {
+    alert("Success");
+  }
+}
+
+submitButton.addEventListener("click", displayDescription);
+
+//Validate assigned to
+
+function validateform() {
+  let assignedTo = document.getElementById("assignedTo");
+
+  if (assignedTo.value.length > 8 || assignedTo.value == "") {
+    alert("Name can't be blank");
+  } else {
+    alert("Success");
+  }
+}
+
+submitButton.addEventListener("click", validateform);
+
+//Due date:
+function dueDates() {
+  var today = new Date();
+  var day = today.getDate();
+  var month = today.getMonth() + 1;
+  let dueDate = document.getElementById("dueDate");
+
+  if (dueDate === " " && dueDate < today) {
+    alert("Select correct Due date");
+  } else {
+    alert("Success");
+  }
+}
+
+submitButton.addEventListener("click", dueDates);
