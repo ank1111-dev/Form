0787038627540a22f4a6717fd0e7f2556a47485a
Ankita More <ankitabtk86@gmail.com>
2022-07-04 12:04:03 +1000

    Updated the script js code

--- resource/js/app.js
+++ resource/js/app.js
@@ -1,52 +1,80 @@
+//Object Instance to the class
+
+let taskManager = new TaskManager(0);
+console.log(taskManager);
+
+//Getting the input values from Form 
 const form = document.getElementById('form');
 const username = document.getElementById('name');
 const descriptionBox = document.getElementById('description');
 const assignedTo = document.getElementById('assignedTo');
 const dueDate = document.getElementById('duedate');
 const validateStatus = document.getElementById('status');
+const submitBtn = document.getElementById('submitBTN');
 
-//Displaying Due Date 
+//Selected due date should be greater then current date
 
-let ToDate = new Date();
- dueDateInput = new Date (dueDate.value);
-//console.log(ToDate);
-//console.log(inpDate);
+let today =  new Date();
+ var dd = ("0" + today.getDate()).slice(-2);
+ var mm = today.getMonth()+1; 
+ var yyyy = today.getFullYear();
+    if(mm<10)  {
+         mm='0'+mm;
+     }
+ today = yyyy + "-" + mm + "-" + dd;
+  
+  console.log(today);
+  console.log(dueDate.value);
+  
+
 
 // Form Validation Logic
+  function submitForm (event) {
 
-form.addEventListener('submit',function(e) { 
+  event.preventDefault(); 
+  console.log(`Form submitted!`);
 
+    
   if (username.value.length <= 8 || username.value === "" ) {
-    e.preventDefault();
+    
     document.getElementById('error-name').innerHTML = "Name should be more then 8 characters";
-  }
+    return false;
+    }
 
   if ( descriptionBox.value.length <= 15 || descriptionBox.value === "") {
-    e.preventDefault();
+   
     document.getElementById('error-description').innerHTML = "Description should be more then 15 characters";
+    return false;
   }
 
   if (assignedTo.value.length <= 8 || assignedTo.value === "" ) {
-    e.preventDefault();
+   
     document.getElementById('error-assignee').innerHTML= 'Assigned to text should be more then 8 characters';
+    return false;
   }
 
 
- // if (dueDate.value == '' || Date.parse(dueDateInput) < ToDate.getTime()) {
-
-  //e.preventDefault();
+  if (dueDate.value == '' || dueDate.value < today) {
   
-  // document.getElementById('error-ddate').innerHTML = "The Date must be Bigger or Equal to today date";
+  document.getElementById('error-ddate').innerHTML = "The Date must be Bigger or Equal to today date";
+  return false;
+  }
 
-  // }
-
-  if (validateStatus.selectedIndex == 0) {
-    e.preventDefault();
+  
+  if (validateStatus.selectedIndex === 0) {
+   
     document.getElementById('error-status').innerHTML = 'Please select an status option';
+    return false;
   }
-  
-  
-});
+ //Adding functions to submit button
+  toAddTasks();
+
+};
+
+//Event handler for submit button
+form.addEventListener("submit",submitForm)
+
+
 
 //Displaying current date on landing page
 function currentDate () {
@@ -61,4 +89,12 @@ function currentDate () {
   el.textContent = date;
 
 }
-currentDate();
+currentDate(); //Calling the current date function
+
+//Function to combine all other function 
+function toAddTasks() {
+  
+  taskManager.addTask(username.value, descriptionBox.value, assignedTo.value, dueDate.value, validateStatus.value);
+  taskManager.render();
+       
+}
--- resource/js/script.js
+++ resource/js/script.js
@@ -1,66 +1,66 @@
-//Validate name
+// //Validate name
 
-function validateName(){
-  let username= document.getElementById('name');
+// function validateName(){
+//   let username= document.getElementById('name');
    
-  if (username.value.length > 8 || username.value == "" ) {
+//   if (username.value.length > 8 || username.value == "" ) {
 
-    alert("Input invalid")
-  } else {
-    alert("Success")
-  }
-}
-let submitButton = document.getElementById("submitBTN");
-submitButton.addEventListener("click", validateName);
+//     alert("Input invalid")
+//   } else {
+//     alert("Success")
+//   }
+// }
+// let submitButton = document.getElementById("submitBTN");
+// submitButton.addEventListener("click", validateName);
 
 
-//Description
+// //Description
 
-function displayDescription() {
+// function displayDescription() {
  
 
-  let descriptionBox = document.getElementById("description");
+//   let descriptionBox = document.getElementById("description");
 
-  if (descriptionBox.value === "" || descriptionBox.value.length > 15) {
-    alert('Please write your description!');
-  } else {
-    alert('Success');
-  }
-}
+//   if (descriptionBox.value === "" || descriptionBox.value.length > 15) {
+//     alert('Please write your description!');
+//   } else {
+//     alert('Success');
+//   }
+// }
 
-submitButton.addEventListener("click", displayDescription);
+// submitButton.addEventListener("click", displayDescription);
 
-//Validate assigned to 
+// //Validate assigned to 
 
-function validateform(){  
-  let assignedTo = document.getElementById('assignedTo');   
+// function validateform(){  
+//   let assignedTo = document.getElementById('assignedTo');   
 
-  if (assignedTo.value.length > 8 || assignedTo.value == "" ) {  
-    alert("Name can't be blank");  
+//   if (assignedTo.value.length > 8 || assignedTo.value == "" ) {  
+//     alert("Name can't be blank");  
     
-  } else  {  
-    alert("Success");  
+//   } else  {  
+//     alert("Success");  
   
-  }  
-}  
+//   }  
+// }  
 
-submitButton.addEventListener('click',  validateform);
+// submitButton.addEventListener('click',  validateform);
 
 
-//Due date:
-function dueDates () {
-  var today = new Date();
-  var day = today.getDate();
-  var month = today.getMonth()+1;
-  let dueDate = document.getElementById("dueDate");
+// //Due date:
+// function dueDates () {
+//   var today = new Date();
+//   var day = today.getDate();
+//   var month = today.getMonth()+1;
+//   let dueDate = document.getElementById("dueDate");
 
-  if ( dueDate === " " && dueDate  < today) {
+//   if ( dueDate === " " && dueDate  < today) {
 
-    alert ("Select correct Due date");
-  } else{
-    alert("Success");
-  }
+//     alert ("Select correct Due date");
+//   } else{
+//     alert("Success");
+//   }
 
-};
+// };
 
-submitButton .addEventListener('click',  dueDates);
+// submitButton .addEventListener('click',  dueDates);
