288544372198b0a515240992e7196b43cbeb3df6
Mahsa <mahsahaddadi19@gmail.com>
2022-07-05 14:42:58 +1000

    no changes

--- resource/js/taskmanager.js
+++ resource/js/taskmanager.js
@@ -1,7 +1,13 @@
 //Creating the card format to display on the browser
 
-const createTaskHtml = (id, username, descriptionBox, assignedTo, dueDate,  validateStatus) => {
-
+const createTaskHtml = (
+  id,
+  username,
+  descriptionBox,
+  assignedTo,
+  dueDate,
+  validateStatus
+) => {
   const newForm = ` 
   <div class="card" style="width: 20rem; margin: 10%; box-shadow: 4px 4px ;" data-task-id=${id}>
 <div class="card-body">
@@ -24,75 +30,77 @@ const createTaskHtml = (id, username, descriptionBox, assignedTo, dueDate,  vali
     </div>
   </div>
 </div>
-</div>`
+</div>`;
 
-
-return newForm;
-  
-}
+  return newForm;
+};
 
 //Creating class to add task
-class TaskManager{
-
-  constructor(currentId =0) {
-      this.tasks = [];
-      this.currentId = currentId;
-      
+class TaskManager {
+  constructor(currentId = 0) {
+    this.tasks = [];
+    this.currentId = currentId;
   }
-//Add the task to array
-  addTask(username, descriptionBox, assignedTo, dueDate,  validateStatus) {
-      const task = {
-        id: this.currentId++,
-        username: username,
-        descriptionBox: descriptionBox,
-        assignedTo: assignedTo,
-        dueDate: dueDate,
-        validateStatus: validateStatus,
-      };
-              
-      this.tasks.push(task);
-      console.log(`$(task.id) \n $(task.username)\n $(task.descriptionBox)\n $(task.assignedTo)\n $(task.dueDate) \n $(task.validateStatus)`);    
-      return task
-    }
+  //Add the task to array
+  addTask(username, descriptionBox, assignedTo, dueDate, validateStatus) {
+    const task = {
+      id: this.currentId++,
+      username: username,
+      descriptionBox: descriptionBox,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      validateStatus: validateStatus,
+    };
 
-    // Getting the task by id
-
-    getTaskById(taskId) {
-
-      let objTask;
-      for (let i = 0; i < this.tasks.length; i++) {
-        const task = this.tasks[i];
-  
-        if (task.id === taskId) {
-          objTask = task;
-        }
-      }
-      
-      return objTask;
-    }
-
-    //Displaying Task function
-
-    render() {
-
-      const tasksHtmlList = [];
-  
-      for (let i = 0; i < this.tasks.length; i++) {
-        const task = this.tasks[i];
-  
-        const date = new Date(task.dueDate);
-        const formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-  
-        const taskHtml = createTaskHtml(task.id, task.username, task.descriptionBox, task.assignedTo, formattedDate, task.validateStatus);
-  
-        tasksHtmlList.push(taskHtml);
-      }
-  
-      const tasksHtml = tasksHtmlList.join("\n");
-  
-      const tasksList = document.getElementById('tasksList');
-      tasksList.innerHTML = tasksHtml;
-    }
-
-   
+    this.tasks.push(task);
+    console.log(
+      `$(task.id) \n $(task.username)\n $(task.descriptionBox)\n $(task.assignedTo)\n $(task.dueDate) \n $(task.validateStatus)`
+    );
+    return task;
   }
+
+  // Getting the task by id
+
+  getTaskById(taskId) {
+    let objTask;
+    for (let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i];
+
+      if (task.id === taskId) {
+        objTask = task;
+      }
+    }
+
+    return objTask;
+  }
+
+  //Displaying Task function
+
+  render() {
+    const tasksHtmlList = [];
+
+    for (let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i];
+
+      const date = new Date(task.dueDate);
+      const formattedDate =
+        date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+
+      const taskHtml = createTaskHtml(
+        task.id,
+        task.username,
+        task.descriptionBox,
+        task.assignedTo,
+        formattedDate,
+        task.validateStatus
+      );
+
+      tasksHtmlList.push(taskHtml);
+    }
+
+    const tasksHtml = tasksHtmlList.join("\n");
+
+    const tasksList = document.getElementById("tasksList");
+    tasksList.innerHTML = tasksHtml;
+  }
+}
